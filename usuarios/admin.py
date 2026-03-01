f100a254a73c74f3fb5a0e50094c385a9e0034ab
Alan Martinez Lomas <alanmartinezlomas@gmail.com>
2026-03-01 13:24:47 -0600

    Base de datos Usuario creada y registro del modelo Usuario al panel de administracion

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Usuario
 # Register your models here.
+
+#Usuario visible en el panel de administracion de django
+admin.site.register(Usuario)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+#Nombre en la base de datos: usuarios_usuario
+class Usuario(AbstractUser):
+    #---Se hereda de un modelo por defecto de django---
+    #---COLUMNAS DE LA TABLA---
+    #id--> se genera automaticamente, es incrementable y PRIMARY KEY
+    # Campos que ya trae AbstractUser:
+    # username, password, first_name (Nombre), last_name (Apellidos), email
+    
+    #Campos adicionales (verbose_name=Nombre que sale en los formularios)
+    cedula = models.CharField(max_length=20,unique=True,verbose_name="Cédula Profesional")#unique=No permite que dos doctores tengan la misma cedula
+    especialidad = models.CharField(max_length=100, verbose_name="Especialidad")
+    telefono = models.CharField(max_length=15, verbose_name="Teléfono de Contacto",blank=True,null=True) #Es opcional
+    es_activo = models.BooleanField(default=True, verbose_name="¿Está trabajando actualmente?")
+    
+    #Util para visualizar datos de manera clara en consola, panel de administrador
+    def __str__(self):
+        return f"Dr. {self.first_name} {self.last_name} - {self.especialidad}"
+    
+    
+#SIEMPRE QUE SE CREA UN MODELO APLICAR
+#python manage.py makemigrations usuarios
+# python manage.py migrate
+
+#---CREDENCIALES---
+#username: admin
+#password: admin2026
