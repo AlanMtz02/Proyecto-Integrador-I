d4696d0728873531c55fbccd42a068ab8fddf0d0
Alan Martinez Lomas <alanmartinezlomas@gmail.com>
2026-03-01 14:13:08 -0600

    Modelo Paciente terminado y agregado al admin

--- clinica/admin.py
+++ clinica/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Paciente
 
-# Register your models here.
+#Registramos y a la vez configuramos la forma que se muestra el modelo en el admin
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    # Columnas
+    list_display = ('nombre', 'apellidos', 'curp', 'es_activo')
+    #Buscador por nombre,apellidos, curp,id
+    search_fields = ('nombre', 'apellidos', 'curp','id')
--- clinica/models.py
+++ clinica/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+#Nombre en la base de dato: clinica_paciente
+class Paciente(models.Model):
+    
+    # Opciones para el Género
+    #El primer valor lo guarda en la base de datos
+    #El segundo valor  es lo que se ve en el HTML o en el panel de admin
+    GENERO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro'),
+    ]
+
+    SANGRE_CHOICES = [
+        ('O+', 'O Positivo'),
+        ('O-', 'O Negativo'),
+        ('A+', 'A Positivo'),
+        ('A-', 'A Negativo'),
+        ('B+', 'B Positivo'),
+        ('B-', 'B Negativo'),
+        ('AB+', 'AB Positivo'),
+        ('AB-', 'AB Negativo'),
+    ]
+    
+    #---COLUMNAS DE LA TABLA---
+    #id-->Se genera automaticamente, es autoincrementable y PRIMARY KEY
+    curp = models.CharField(max_length=18, unique=True, verbose_name="CURP") #Es unico (no se puede repetir)
+    nombre = models.CharField(max_length=100, verbose_name="Nombre(s)")
+    apellidos = models.CharField(max_length=100, verbose_name="Apellidos")
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES, verbose_name="Género")#Solo 1 respuesta y las opciones marcadas
+    direccion = models.TextField(verbose_name="Dirección Completa")
+    telefono = models.CharField(max_length=15, verbose_name="Teléfono",blank=True,null=True)#Opcional
+    tipo_sangre = models.CharField(max_length=3,choices=SANGRE_CHOICES, verbose_name="Tipo de Sangre")
+    alergias = models.TextField(verbose_name="Alergias", blank=True, null=True)  # Opcional
+    es_activo = models.BooleanField(default=True, verbose_name="Estatus Activo")
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos} ({self.curp})"
