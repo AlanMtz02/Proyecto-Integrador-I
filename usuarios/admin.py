892d32d3f3a9ce80e054432b39148143e44a9a8b
Alan Martinez Lomas <alanmartinezlomas@gmail.com>
2026-03-02 19:38:41 -0600

    Agregar campos del doctor al admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,12 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
+
 class UsuarioAdmin(UserAdmin):
-    # Esto hace que la cédula y especialidad salgan en el formulario de edición
+    # Columnas
+    list_display = ('username', 'first_name', 'last_name',
+                    'cedula', 'especialidad', 'is_staff')
+
+    # Esto es lo que el formulario para meter los campos nuevos
+    # Agregamos una sección llamada 'Información Médica'
     fieldsets = UserAdmin.fieldsets + (
-        ('Información Médica (Solo para Doctores)', {'fields': ('cedula', 'especialidad', 'telefono', 'es_activo')}),
+        ('Información Médica (Datos del Doctor)', {
+            'fields': ('cedula', 'especialidad', 'telefono', 'es_activo')
+        }),
     )
-    # Esto hace que se vean en la lista principal
-    list_display = ('username', 'first_name', 'last_name', 'cedula', 'is_staff')
+
+    # Campos al crear un usuario nuevo 
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Información Médica', {
+            'fields': ('cedula', 'especialidad', 'telefono', 'es_activo')
+        }),
+    )
+
 
 admin.site.register(Usuario, UsuarioAdmin)
